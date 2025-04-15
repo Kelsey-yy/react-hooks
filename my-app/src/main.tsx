0da49284eae8541ce21da6afb67c3dc999c8a6a6
hongyanfang <hyfpika@gmail.com>
2025-04-15 08:51:26 +0800

    feat: 搭建主页面

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from '@/router/index'
 import { Provider } from 'react-redux'
 import store from '@/store'
+import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
--- my-app/src/pages/Layout/index.tsx
+++ my-app/src/pages/Layout/index.tsx
@@ -1,15 +1,76 @@
-import {request} from '@/utils'
-import { useEffect } from 'react'
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout as _Layout, Menu, theme } from 'antd';
+
+const { Header, Sider, Content } = _Layout;
 
 const Layout = () => {
-    useEffect(() => {
-        request.get('/user/profile')
-    })
-    return (
-        <div>
-            layout
-        </div>
-    )
-}
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <_Layout style={{ minHeight: '100vh' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: '首页',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: '文章管理',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: '创建文章',
+            },
+          ]}
+        />
+      </Sider>
+      <_Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </_Layout>
+    </_Layout>
+  );
+};
+
 
 export default Layout
