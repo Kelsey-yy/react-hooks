a60f5d958cdfeb32ec9e9d24e3d32d5acb48304d
hongyanfang <hyfpika@gmail.com>
2025-04-15 09:52:59 +0800

    feat: 1.调整布局2.获取用户信息

--- my-app/src/pages/Layout/index.tsx
+++ my-app/src/pages/Layout/index.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+    LogoutOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import { Button, Layout as _Layout, Menu, theme } from 'antd';
+import { Button, Layout as _Layout, Menu, theme, Popconfirm } from 'antd';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserInfo } from '@/store/module/user';
+import './index.scss'
 
 const { Header, Sider } = _Layout;
 const items = [
@@ -45,37 +49,64 @@ const Layout = () => {
   const  selectedKey = location.pathname;
   
 
+  const dispatch = useDispatch();
+  // 获取个人信息
+  useEffect(() => {
+    dispatch(fetchUserInfo())
+  }, [dispatch])
+
+  const name = useSelector((state: any) => state.user.userInfo.name)
 
   return (
     <_Layout style={{ minHeight: '100vh' }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          selectedKeys={selectedKey}
-          items={items}
-          onClick={onMenuClick}
-        />
-      </Sider>
-      <_Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+        <Header style={{ display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
+            <div className="logo" />
+            <div className='user-info'>
+                <span className='user-name'>{name}</span>
+                <span className='user-logout'>
+                <Popconfirm
+                    title="是否确定退出?"
+                    okText="退出"
+                    cancelText="取消"
+                >
+                    <LogoutOutlined style={{marginRight: '10px'}} />
+                    退出
+                </Popconfirm>
+                </span>
+            </div>
+        
         </Header>
         <_Layout>
-            {/* 二级路由出口 */}
-            <Outlet />
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="demo-logo-vertical" />
+                <Menu
+                theme="dark"
+                mode="inline"
+                selectedKeys={selectedKey}
+                items={items}
+                onClick={onMenuClick}
+                />
+            </Sider>
+            <_Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                <Button
+                    type="text"
+                    icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                    onClick={() => setCollapsed(!collapsed)}
+                    style={{
+                    fontSize: '16px',
+                    width: 64,
+                    height: 64,
+                    }}
+                />
+                </Header>
+                <_Layout>
+                    {/* 二级路由出口 */}
+                    <Outlet />
+                </_Layout>
+            </_Layout>
         </_Layout>
-      </_Layout>
+      
     </_Layout>
   );
 };
--- my-app/src/store/module/user.ts
+++ my-app/src/store/module/user.ts
@@ -3,35 +3,48 @@ import { request, setToken as _setToken, getToken } from "@/utils";
 const userStore = createSlice({
     name: 'user',
     initialState: {
-        token: getToken() || ''
+        token: getToken() || '',
+        userInfo: {}
     },
     reducers: {
         setToken(state, action) {
             state.token = action.payload
             _setToken(action.payload)
+        },
+        setUserInfo(state, action) {
+            state.userInfo = action.payload
         }
     }
 })
 
 // 结构出actionCreater
 
-const  { setToken } = userStore.actions
+const  { setToken, setUserInfo } = userStore.actions
 
 // 获取reducer函数
 const userReducer = userStore.reducer
 
 // 异步方法获取token
-
 const fetchLogin = (LoginForm: any) => {
     return async (dispatch: any) => {
         // 1. 发送请求获取token
         // 2.提交同步action，进行token的存入
-        
         const res = await request.post('/authorizations', LoginForm) // 1.
         dispatch(setToken(res.data.token)) // 2.
     }
 }
 
-export { setToken, fetchLogin }
+// 异步方法获取用户信息
+const fetchUserInfo = () => {
+    return async (dispatch: any) => {
+       const res = await request.get('/user/profile')
+       dispatch(setUserInfo(res.data))
+    }
+}
+
+
+
+
+export { setToken, fetchLogin, fetchUserInfo }
 
 export default userReducer // 用来再store里面去组装的
